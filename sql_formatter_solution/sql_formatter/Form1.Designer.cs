d070cf817a14f3f9750eb628e48f6567cccf83c7
Diogo Alpendre <37402115+dalpendre@users.noreply.github.com>
2021-04-13 09:35:25 +0100

    Code added v1

---
+++ sql_formatter_solution/sql_formatter/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace sql_formatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFields = new System.Windows.Forms.TextBox();
+            this.buttonCreateSQLCommand = new System.Windows.Forms.Button();
+            this.buttonReadFile = new System.Windows.Forms.Button();
+            this.buttonDeleteTextBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxFields
+            // 
+            this.textBoxFields.Location = new System.Drawing.Point(13, 13);
+            this.textBoxFields.Multiline = true;
+            this.textBoxFields.Name = "textBoxFields";
+            this.textBoxFields.Size = new System.Drawing.Size(369, 426);
+            this.textBoxFields.TabIndex = 0;
+            // 
+            // buttonCreateSQLCommand
+            // 
+            this.buttonCreateSQLCommand.Location = new System.Drawing.Point(12, 451);
+            this.buttonCreateSQLCommand.Name = "buttonCreateSQLCommand";
+            this.buttonCreateSQLCommand.Size = new System.Drawing.Size(168, 23);
+            this.buttonCreateSQLCommand.TabIndex = 1;
+            this.buttonCreateSQLCommand.Text = "Create SQL Command String";
+            this.buttonCreateSQLCommand.UseVisualStyleBackColor = true;
+            this.buttonCreateSQLCommand.Click += new System.EventHandler(this.buttonCreateSQLCommand_Click);
+            // 
+            // buttonReadFile
+            // 
+            this.buttonReadFile.Location = new System.Drawing.Point(186, 451);
+            this.buttonReadFile.Name = "buttonReadFile";
+            this.buttonReadFile.Size = new System.Drawing.Size(196, 23);
+            this.buttonReadFile.TabIndex = 2;
+            this.buttonReadFile.Text = "Read from file";
+            this.buttonReadFile.UseVisualStyleBackColor = true;
+            this.buttonReadFile.Click += new System.EventHandler(this.buttonReadFile_Click);
+            // 
+            // buttonDeleteTextBox
+            // 
+            this.buttonDeleteTextBox.Location = new System.Drawing.Point(12, 480);
+            this.buttonDeleteTextBox.Name = "buttonDeleteTextBox";
+            this.buttonDeleteTextBox.Size = new System.Drawing.Size(369, 23);
+            this.buttonDeleteTextBox.TabIndex = 3;
+            this.buttonDeleteTextBox.Text = "Delete Text Box";
+            this.buttonDeleteTextBox.UseVisualStyleBackColor = false;
+            this.buttonDeleteTextBox.Click += new System.EventHandler(this.buttonDeleteTextBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 515);
+            this.Controls.Add(this.buttonDeleteTextBox);
+            this.Controls.Add(this.buttonReadFile);
+            this.Controls.Add(this.buttonCreateSQLCommand);
+            this.Controls.Add(this.textBoxFields);
+            this.Name = "Form1";
+            this.Text = "SQL Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFields;
+        private System.Windows.Forms.Button buttonCreateSQLCommand;
+        private System.Windows.Forms.Button buttonReadFile;
+        private System.Windows.Forms.Button buttonDeleteTextBox;
+    }
+}
+
---
+++ sql_formatter_solution/sql_formatter/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sql_formatter
+{
+    public partial class Form1 : Form
+    {
+        private String command = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreateSQLCommand_Click(object sender, EventArgs e)
+        {
+            //Separates each line
+            String[] command_fields =
+            textBoxFields.Text.Split(new Char[] { '\n', '\r' }, 
+            StringSplitOptions.RemoveEmptyEntries); ;
+
+            //Foreach line adds to command
+            foreach(String field in command_fields)
+            {
+                command += field + ", ";
+            }
+
+            textBoxFields.AppendText(Environment.NewLine);
+            textBoxFields.AppendText(Environment.NewLine);
+            textBoxFields.AppendText(command);
+        }
+
+        private void buttonReadFile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Only reads text and spreadsheet (excel) files");
+
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if(openFileDialog.ShowDialog() == DialogResult.OK) 
+                {
+                    String file_extension = Path.GetExtension(openFileDialog.FileName);
+
+                    switch(file_extension)
+                    {
+                        case ".txt":
+                            read_txt_file(openFileDialog.FileName);
+                            break;
+
+                        /*default:
+                            MessageBox.Show("Invalid file extension!");
+                            break;*/
+                    }
+                }
+            }
+        }
+
+        private void read_txt_file(String filename) 
+        {
+            foreach(String field in File.ReadLines(filename))
+            {
+                command += field + ", ";
+            }
+
+            textBoxFields.AppendText(command);
+        }
+
+        private void buttonDeleteTextBox_Click(object sender, EventArgs e)
+        {
+            textBoxFields.Text = null;
+        }
+    }
+}
